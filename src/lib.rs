dbf435e87f3c030402c3a59b6497e61213fec88c
Yothin Muangsommuk <yothin.m@linecorp.com>
2023-09-02 12:31:33 +0700

    Restructure project to lib, so that it can refer and testable

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use actix_web::{web, App, HttpServer, HttpResponse};
+
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub async fn run() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+            App::new()
+                .route("/health_check", web::get().to(health_check))
+        })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use zero2prod::run;
 
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    run().await
 }
